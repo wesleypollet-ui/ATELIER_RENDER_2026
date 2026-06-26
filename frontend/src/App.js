c8a30cf8223c93a0940804a11f0210833207389f
wesleypollet-ui <wesley.pollet@2028.icam.fr>
2026-06-26 11:06:22 +0200

    Update paragraph text in App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div>
       <h1>Bienvenue sur mon application React !</h1>
-      <p>Atelier Render 2026</p>
+      <p>Atelier Render 2026 - Wesley POLLET</p>
     </div>
   );
 }
